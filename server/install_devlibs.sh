28607810fcfd39db0c5c07de711007874bf191a8
yxb <xiangbo_x@qq.com>
2021-11-18 09:00:53 +0800

    update install nginx

---
+++ server/install_devlibs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt-get update && apt-get install -y \
+    curl \
+    vim \
+    procps \
+    libssl-dev \
+    libreadline-dev \
+    libcurl4-openssl-dev 
--- server/install_nginx_php_mysql.sh
+++ server/install_nginx_php_mysql.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-apt-get update && apt-get install -y \
-    curl \
-    vim \
-    procps \
-    libssl-dev \
-    libreadline-dev \
-    libcurl4-openssl-dev 
-
 apt-get install -y \
     mariadb-server \
     mariadb-client \
@@ -14,9 +6,6 @@ apt-get install -y \
     php-fpm \
     php-bz2 php-cli php-curl php-gd php-json php-mbstring php-mysql php-readline php-xml php-zip \
     php-bcmath php-common php-tokenizer \
-    nginx \
-    coturn \
-    cron \
-    certbot python-certbot-nginx
+    nginx 
 
 
